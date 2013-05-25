245eb5b4ec4281835e609de119805682e5332cc2
codegambler <codegambler@gmail.com>
2013-05-26 03:01:04 +0530

    few more beautification

--- src/main/scala/code/model/User.scala
+++ src/main/scala/code/model/User.scala
@@ -40,14 +40,57 @@ object User extends User with MetaMegaProtoUser[User] {
     (<form method="post" action={ S.uri } class="form-signin">
        <table>
          <tr><td colspan="2"><h3 class="form-signin-heading">{ S.?("log.in") }</h3></td></tr>
-         <tr><td><input type="email" name="username" class="input-block-level span3" placeholder={ S.?("email.address") } required="" maxlength="48"/></td></tr>
-         <tr><td><input type="password" name="password" class="input-block-level span3" placeholder={ S.?("password") } required="" maxlength="48"/></td></tr>
+         <tr><td><input type="email" name="username" class="input-block-level span3" placeholder={ S.?("email.address") } maxlength="48"/></td></tr>
+         <tr><td><input type="password" name="password" class="input-block-level span3" placeholder={ S.?("password") } maxlength="48"/></td></tr>
          <tr>
            <td>
              <user:submit/>
              <span class="pull-right"><a href={ lostPasswordPath.mkString("/", "/", "") }> { S.?("recover.password") }</a> </span>
            </td>
          </tr>
+       </table>
+     </form>)
+  }
+
+  override def editXhtml(user: TheUserType) = {
+    (<form method="post" action={ S.uri } class="form-signin">
+       <table>
+         <tr><td colspan="2"><h3 class="form-signin-heading">{ S.?("edit") }</h3></td></tr>
+         { localForm(user, true, editFields) }
+         <tr><td><user:submit/></td></tr>
+       </table>
+     </form>)
+  }
+
+  override def lostPasswordXhtml = {
+    (<form method="post" action={ S.uri } class="form-signin">
+       <table>
+         <tr><td colspan="2"> <h3 class="form-signin-heading"> { S.?("enter.email") } </h3></td></tr>
+         <tr><td>{ userNameFieldString }</td><td><user:email/></td></tr>
+         <tr><td><user:submit/></td></tr>
+       </table>
+     </form>)
+  }
+
+  override def passwordResetXhtml = {
+    (<form method="post" action={ S.uri } class="form-signin">
+       <table>
+         <tr><td colspan="2"> <h3 class="form-signin-heading">{ S.?("reset.your.password") } </h3></td></tr>
+         <tr><td>{ S.?("enter.your.new.password") }</td><td><user:pwd/></td></tr>
+         <tr><td>{ S.?("repeat.your.new.password") }</td><td><user:pwd/></td></tr>
+         <tr><td><user:submit/></td></tr>
+       </table>
+     </form>)
+  }
+
+  override def changePasswordXhtml = {
+    (<form method="post" action={ S.uri }>
+       <table>
+         <tr><td colspan="2">{ S.?("change.password") }</td></tr>
+         <tr><td>{ S.?("old.password") }</td><td><user:old_pwd/></td></tr>
+         <tr><td>{ S.?("new.password") }</td><td><user:new_pwd/></td></tr>
+         <tr><td>{ S.?("repeat.password") }</td><td><user:new_pwd/></td></tr>
+         <tr><td><user:submit/></td></tr>
        </table>
      </form>)
   }
@@ -78,7 +121,7 @@ class User extends MegaProtoUser[User] with CreatedUpdated {
     override def _toForm: Box[Elem] =
       S.fmapFunc({ s: List[String] => this.setFromAny(s) }) { name =>
         Full(appendFieldId(
-          <input type={ formInputType } maxlength={ maxLen.toString } name={ name } value={ is match { case null => "" case s => s.toString } } class="input-block-level span3" placeholder={ S.?("email.address") } required=""/>))
+          <input type={ formInputType } maxlength={ maxLen.toString } name={ name } value={ is match { case null => "" case s => s.toString } } class="input-block-level span3" placeholder={ S.?("email.address") }/>))
       }
   }
 
@@ -88,8 +131,8 @@ class User extends MegaProtoUser[User] with CreatedUpdated {
     override def _toForm: Box[NodeSeq] = {
       S.fmapFunc({ s: List[String] => this.setFromAny(s) }) { funcName =>
         Full(<span>
-               { appendFieldId(<input type={ formInputType } name={ funcName } value="" class="input-block-level span3" placeholder={ displayName } required=""/>) }
-               <input type={ formInputType } name={ funcName } value="" class="input-block-level span3" placeholder={ S.?("repeat") + " " + displayName } required=""/>
+               { appendFieldId(<input type={ formInputType } name={ funcName } value="" class="input-block-level span3" placeholder={ displayName }/>) }
+               <input type={ formInputType } name={ funcName } value="" class="input-block-level span3" placeholder={ S.?("repeat") + " " + displayName }/>
              </span>)
       }
     }
